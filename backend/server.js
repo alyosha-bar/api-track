cbdb383add307cf3500496f2935a21198e28e498
Alyosha <alohahoy@gmail.com>
2025-07-21 18:04:50 +0100

    Im a dumbass - new client URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "https://diligent-perception-production.up.railway.app",
+    "https://aware-perfection-production.up.railway.app",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
